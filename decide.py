f05847214e991f4a11311b98c88f137aacf19ed8
François <fran07690@gmail.com>
2019-11-25 15:43:55 +0100

    pythonification

--- decide.py
+++ decide.py
@@ -5,7 +5,6 @@
 """
 from numpy import linalg as LA
 from delay import delay
-import copy
 
 
 def enemy_of(unit):
@@ -40,33 +39,32 @@ def direction(unit, other):
     return delta / LA.norm(delta)
 
 
-def focus(unit, target, fleeing_prob):
+def focus(unit, target):
     """Focus on one specific enemy.
     If within reach attack, otherwise approach.
     """
-    unit.flee(fleeing_prob)
-
     if distance_from(unit)(target) <= unit.reach:
         return unit.attack(target)
     return unit.move(direction(unit, target))
 
 
 def has_coward(unit, allies, enemies):
-    """Return true if this unit may have
-    fleeing individuals, false else"""
-    dis = sum(distance_from(unit)(enemy)
-              for enemy in enemies if not enemy.is_dead)
-    dis_allies = sum(sum(distance_from(ally)(enemy)
-                      for enemy in enemies if not enemy.is_dead) for ally in allies)
-    return 2*dis/dis_allies
+    """Units further from the enemy might have fleeing issues.
+    Return probability of such issues.
+    """
+    def distance_to_enemies(_unit):
+        return sum(distance_from(_unit)(enemy) for enemy in enemies)
+
+    remote = distance_to_enemies(unit)
+    remote_allies = sum(distance_to_enemies(ally) for ally in allies)
+    return 2 * remote / remote_allies
 
 
 def strategy(distance=0, health=0):
     """Order are based on preference between proximity and weakness."""
     def order(unit, others):
-        others2 = copy.deepcopy(others)
         allies = list(filter(ally_of(unit), others))
-        enemies = list(filter(enemy_of(unit), others2))
+        enemies = list(filter(enemy_of(unit), others))
         if not enemies or unit.is_dead:
             return delay(lambda: None)
 
@@ -74,8 +72,9 @@ def strategy(distance=0, health=0):
             return distance * distance_from(unit)(other) + health * unit.health
         # todo: normalization
 
+        unit.flee(has_coward(unit, allies, enemies))
         target = sorted(enemies, key=criteria)[0]
-        return focus(unit, target, has_coward(unit, allies, enemies))
+        return focus(unit, target)
 
     return order
 
