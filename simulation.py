055dedc2f42444afe3669ae9a741ed549e858087
François <fran07690@gmail.com>
2020-01-17 09:31:55 +0100

    delay fix

--- simulation.py
+++ simulation.py
@@ -1,3 +1,4 @@
+"""Run and manage simulations, file I/O."""
 from copy import deepcopy
 from dataclasses import dataclass
 from itertools import islice
@@ -10,22 +11,28 @@ from tools import tools, Cache
 
 @dataclass
 class Simulation:
+    """Container for successive steps of a battle."""
     states: List[List[Unit]]
 
     @property
     def size(self):
+        """Length of battle (number of steps)."""
         return len(self.states)
 
     @property
     def units(self):
+        """Access units from latest state of the battle."""
         return self.states[-1]
 
     @property
     def volume(self):
+        """Total health on each side."""
+        return [min([u.health for u in self.units if u.side == s]) for s in (0, 1)]
         return [sum([u.health for u in self.units if u.side == s]) for s in (0, 1)]
 
     @tools(log="{self.volume}")
     def update(self):
+        """Generate and append new state to simulation."""
         Cache.reset()
         self.states.append(deepcopy(self.units))
         sum((unit.decide(self.units) for unit in self.units), None)()
@@ -33,13 +40,13 @@ class Simulation:
 
     @property
     def is_finished(self):
-        health = np.zeros(2, int)
-        for unit in self.units:
-            health[unit.side] += unit.health
-        return np.all(health > 0)
+        """Tell whether battle is over (one side has no health)."""
+        return np.all([sum([u.health for u in self.units if u.side == s]) for s in (0, 1)])
+        return np.all(self.volume)
 
 
 def prepare_battle():
+    """Generate initial state of custom battle."""
     infantryman = UnitBase(4, 1.5, 1, 5)
     centurion = UnitBase(100, 1.5, 1, 1000)
     strategies = [Strategy(1, 0), Strategy(0, 1)]
@@ -60,6 +67,7 @@ def prepare_battle():
 
 
 def read_battle(file_name):
+    """Parse file for successive steps of a battle."""
     with open(file_name, 'r') as file:
         lines_strat = [file.readline().split() for _ in range(2)]
         strategies = np.array(lines_strat, int)
@@ -93,6 +101,7 @@ def read_battle(file_name):
 
 
 def make_battle(init, file_name: str):
+    """Generate battle from initial state and write it to file."""
     with open(file_name, 'w') as file:
         file.write("1 0\n")
         file.write("0 1\n")
